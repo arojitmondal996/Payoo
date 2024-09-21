751fbdb972714be64fd8d6e09ab15f0e531eabf5
arojitmondal996e <arojitmondal996@gmail.com>
2024-09-21 10:18:24 +0600

    login section password input

---
+++ js/login.js
@@ -0,0 +1,17 @@
+// console.log('button clicking file added')
+
+// document.getElementById('id').addEventListener('click', fun)
+// document.getElementById('button-login')
+//         .addEventListener('click', function(){})
+// search: form submit reloading the page
+// step: 1 set event handler
+document.getElementById('button-login')
+        .addEventListener('click', function(event){
+            // step: 2 prevent default behavior (prevent reloading browser)
+            event.preventDefault();
+            console.log('log in button clicked');
+
+            // step:3 get the phone number
+            const phoneNumber = document.getElementById('phone-number').value;
+            console.log(phoneNumber)
+        })
